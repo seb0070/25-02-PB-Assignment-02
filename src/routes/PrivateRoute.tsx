5cb1f4c8a8cba318db40c73759f4430c3d17c90e
jsb <tpqls1703@gmail.com>
2025-12-17 06:22:53 +0900

    feat: 인증 기반 PrivateRoute 가드 추가

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { isLoggedIn } from '../utils/authState';
 
 const PrivateRoute = () => {
-    const isLoggedIn = localStorage.getItem('auth') === 'true';
-    return isLoggedIn ? <Outlet /> : <Navigate to="/signin" replace />;
+    return isLoggedIn() ? <Outlet /> : <Navigate to="/signin" replace />;
 };
 
 export default PrivateRoute;
