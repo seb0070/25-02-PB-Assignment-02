13cb2dc2aac6cc6fdd95072dc8fe2d4dc6f22e7f
jsb <tpqls1703@gmail.com>
2025-12-17 23:43:26 +0900

    chore: GitHub Pages 배포를 위한 Vite base 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: '/pb-assignment-02-react/', // 👉 레포 이름 정확히!
+});
