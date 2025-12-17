b1bba36d5fabd92046bb5982f7388f09d207cbe7
jsb <tpqls1703@gmail.com>
2025-12-17 20:18:38 +0900

    feat: 로컬 스토리지 기반 Wishlist 페이지 구현

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,2 +1,20 @@
-const Wishlist = () => <div>Wishlist Page</div>;
+import { useWishlist } from '../hooks/useWishlist';
+
+const Wishlist = () => {
+    const { wishlist } = useWishlist();
+
+    if (wishlist.length === 0) {
+        return <p>찜한 영화가 없습니다.</p>;
+    }
+
+    return (
+        <div>
+            <h2>내가 찜한 영화</h2>
+            {wishlist.map((movie) => (
+                <p key={movie.id}>{movie.title}</p>
+            ))}
+        </div>
+    );
+};
+
 export default Wishlist;
