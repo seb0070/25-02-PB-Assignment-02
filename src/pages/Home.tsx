bc0f889fbbed7fac3de95ee4c205f05c81a14149
jsb <tpqls1703@gmail.com>
2025-12-17 14:24:49 +0900

    feat: Home 페이지에서 인기 영화 API 테스트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,2 +1,24 @@
-const Home = () => <div>Home Page</div>;
+import { useEffect, useState } from 'react';
+import { getPopularMovies } from '../api/movies';
+import { Movie } from '../models/movie';
+
+const Home = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        getPopularMovies().then((res) => {
+            setMovies(res.data.results);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Popular Movies</h1>
+            {movies.map((movie) => (
+                <p key={movie.id}>{movie.title}</p>
+            ))}
+        </div>
+    );
+};
+
 export default Home;
