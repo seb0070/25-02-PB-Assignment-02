ae107597828b7f1c741be7d0cce32da970285a3c
jsb <tpqls1703@gmail.com>
2025-12-17 19:51:38 +0900

    feat: 찜한 영화 관리를 위한 useWishlist 커스텀 훅 구현

---
+++ src/hooks/useWishlist.ts
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Movie } from '../models/movie';
+import { storage } from '../utils/storage';
+import { STORAGE_KEYS } from '../constants/storageKeys';
+
+type WishlistMovie = Pick<Movie, 'id' | 'title' | 'poster_path'>;
+
+export const useWishlist = () => {
+    const [wishlist, setWishlist] = useState<WishlistMovie[]>(
+        storage.get<WishlistMovie[]>(STORAGE_KEYS.WISHLIST, [])
+    );
+
+    const isWished = (id: number) =>
+        wishlist.some((movie) => movie.id === id);
+
+    const toggleWishlist = (movie: WishlistMovie) => {
+        const updated = isWished(movie.id)
+            ? wishlist.filter((item) => item.id !== movie.id)
+            : [...wishlist, movie];
+
+        setWishlist(updated);
+        storage.set(STORAGE_KEYS.WISHLIST, updated);
+    };
+
+    return {
+        wishlist,
+        isWished,
+        toggleWishlist,
+    };
+};
