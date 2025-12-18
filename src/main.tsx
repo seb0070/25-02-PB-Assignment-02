dbbf53f31808291e26df036ee8434876f3b35ab7
jsb <tpqls1703@gmail.com>
2025-12-19 04:56:46 +0900

    GitHub Pages 배포를 위한 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import './styles/global.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+).render(
+    <React.StrictMode>
+        <BrowserRouter basename="/25-02-PB-Assignment-02">
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
