a0c6aa3c72dce8b704d211e8cbfdbb99d5eedb9d
jsb <tpqls1703@gmail.com>
2025-12-19 21:59:34 +0900

    feat: MovieCard에 hover 액션 및 찜하기 기능 추가

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,25 +1,52 @@
-import './MovieCard.css';
 import type { Movie } from '../models/movie';
+import { useWishlist } from '../hooks/useWishlist';
+import { useNavigate } from 'react-router-dom';
+import './MovieCard.css';
 
-type Props = {
+interface MovieCardProps {
     movie: Movie;
-    rank?: number;
-};
+}
 
-const IMAGE_BASE = 'https://image.tmdb.org/t/p/w300';
+const MovieCard = ({ movie }: MovieCardProps) => {
+    const navigate = useNavigate();
+    const { isWished, toggleWishlist } = useWishlist();
 
-export default function MovieCard({ movie, rank }: Props) {
+    const wished = isWished(movie.id);
+
+    const handleWishlist = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        toggleWishlist(movie);
+    };
+
+    const handleInfo = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        navigate(`/movie/${movie.id}`);
+    };
+
     return (
-        <div className="movie-card">
-            {rank && <div className="movie-rank">{rank}</div>}
+        <div
+            className={`movie-card ${wished ? 'wished' : ''}`}
+            onClick={handleInfo}
+        >
             <img
                 src={
                     movie.poster_path
-                        ? `${IMAGE_BASE}${movie.poster_path}`
-                        : '/no-image.png'
+                        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                        : '/no-poster.png'
                 }
                 alt={movie.title}
             />
+
+            <div className="overlay">
+                <div className="actions">
+                    <button onClick={handleWishlist}>
+                        {wished ? '❤️' : '🤍'}
+                    </button>
+                    <button onClick={handleInfo}>ℹ</button>
+                </div>
+            </div>
         </div>
     );
-}
+};
+
+export default MovieCard;
